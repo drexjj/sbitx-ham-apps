02f74bb754440e7dc89f26ca5705b5d39167d35c
jj <155218248+drexjj@users.noreply.github.com>
2025-02-19 23:51:59 -0600

    Update install-wsjtx271.sh

--- install-wsjtx271.sh
+++ install-wsjtx271.sh
@@ -7,19 +7,19 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Update package list
-apt update
+sudo apt update
 
 # Install required dependencies
-apt install -y libqt5multimedia5 libqt5serialport5 libqt5network5 libfftw3-single3 libboost-all-dev
+sudo apt install -y libqt5multimedia5 libqt5serialport5 libqt5network5 libfftw3-single3 libboost-all-dev
 
 # change directory
 cd wsjtx
 
 # Install WSJT-X package
-dpkg -i wsjtx-2.7.1-devel_improved_AL_PLUS_241014-RC7_Rpi_arm64.deb
+sudo dpkg -i wsjtx-2.7.1-devel_improved_AL_PLUS_241014-RC7_Rpi_arm64.deb
 
 # Fix any missing dependencies
-apt --fix-broken install -y
+sudo apt --fix-broken install -y
 
 # Verify installation
 if dpkg -l | grep -q wsjtx; then
