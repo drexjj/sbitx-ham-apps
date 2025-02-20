d9333389e236e413fd55a200d47bfa7f542ff9e2
jj <155218248+drexjj@users.noreply.github.com>
2025-02-20 00:03:29 -0600

    Update install-wsjtx271.sh

--- install-wsjtx271.sh
+++ install-wsjtx271.sh
@@ -2,6 +2,7 @@
 
 # Ensure the script is run with sudo
 if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root or use sudo"
     exec sudo "$0" "$@"
     exit 1
 fi
@@ -10,18 +11,9 @@ fi
 apt update
 
 # Install required dependencies
-apt install -y \
-    libgfortran5 libgfortran4 libgfortran3 libfftw3-single3 libgomp1 \
-    libqt5serialport5 libqt5multimedia5-plugins libqt5widgets5 libqt5network5 \
-    libqt5printsupport5 libqt5sql5-sqlite libusb-1.0-0 \
-    libboost-log1.62.0 libboost-log1.65.1 libboost-log1.67.0 libboost-log1.71.0 \
-    libboost-log1.74.0 libboost-filesystem1.74.0 libboost-thread1.74.0 \
-    libc6 libfftw3-single3 libgcc-s1 libgfortran5 libgomp1 libqt5core5a \
-    libqt5gui5 libqt5gui5-gles libqt5multimedia5 libqt5network5 \
-    libqt5printsupport5 libqt5serialport5 libqt5sql5 libqt5widgets5 \
-    libstdc++6 libusb-1.0-0
+apt install -y libqt5multimedia5 libqt5serialport5 libqt5network5 libfftw3-single3 libboost-all-dev
 
-# change directory
+#change directory
 cd wsjtx
 
 # Install WSJT-X package
